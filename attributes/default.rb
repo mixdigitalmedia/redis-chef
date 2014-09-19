d8228d10e50099cb3f76b93af51111d311000fd2
Kenton Newby <kentonnewby@gmail.com>
2014-09-19 15:00:13 -0400

    initial commit

---
+++ attributes/default.rb
@@ -0,0 +1,8 @@
+#
+# Author:: Mix Digital Media, LLC
+# Cookbook Name:: redis-chef
+# Attributes:: default
+#
+
+# whethere to prevent redis from binding to 127.0.0.1
+default[:redis][:dont_bind] = false
---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name              "redis-chef"
+maintainer        "Mix Digital Media, LLC" # originally inspired by Ben Dixon
+maintainer_email  "info@mixdigitalmedia.com"
+description       "Installs redis from rwky's ppa"
+version           "0.0.1"
+
+recipe "redis-chef", "Installs redis"
+
+supports "ubuntu"
